f73ccd0c938deeed4eff9e021ecdb13f4907365a
lyolichek <lyolya.sashevna@gmail.com>
2018-02-25 13:18:17 +0200

    fix

--- js/pictures.js
+++ js/pictures.js
@@ -253,6 +253,7 @@ buttonInc.addEventListener('click', function () {
 
 var HASHTAG_ERRORS = {
   'symbol': 'Отсутствует обязательный символ #',
+  'symbol_wrong': 'Символ # должен стоять в начале хештега',
   'max': 'Максимальное кол-во хештегов должно быть 5',
   'same': 'Есть повторяющиеся хештеги',
   'maxLength': 'Слишком длинный хештег',
@@ -264,16 +265,17 @@ var inputHashtags = formHashtags.querySelector('.upload-form-hashtags');
 
 // Проверка есть ли аттрибут required у поля с хештегами
 function setRequiredHashtags() {
-  if (inputHashtags.getAttribute('required') === null) {
+  if (inputHashtags.getAttribute('required') == null) {
     inputHashtags.setAttribute('required', 'required');
   }
 }
 
 function checkHashtag(arr) {
+  if (arr.length > 5) {
+    return 'max';
+  }
+
   for (var i = 0; i < arr.length; i++) {
-    if (arr.length > 5) {
-      return 'max';
-    }
     if (arr[i].length > 20) {
       return 'maxLength';
     }
@@ -283,20 +285,35 @@ function checkHashtag(arr) {
     if (arr[i].indexOf('#') < 0) {
       return 'symbol';
     }
+    if (arr[i].indexOf('#') > 0) {
+      return 'symbol_wrong';
+    }
     for (var j = 0; j < arr.length; j++) {
-      if (arr[i] === arr[j]) {
+      if ((toUpperCase(arr[i]) == toUpperCase(arr[j])) && (i !== j)) {
         return 'same';
       }
     }
   }
 }
 
-formHashtags.addEventListener('submit', function (evt) {
-  var hashtagsArr = inputHashtags.value.split(' ');
-  var errorCode = checkHashtag(hashtagsArr);
+function getValidHashtags(hashArr) {
+  var validArr = [];
+  for (var i = 0; i < hashArr.length; i++) {
+    if (hashArr[i] !== '') {
+      validArr.push(hashArr[i]);
+    }
+  }
 
-  evt.preventDefault();
-  //evt.stopPropagation();
+  return validArr;
+}
+
+formHashtags.addEventListener('invalid', function (evt) {
+  console.log('invalid');
+});
+
+formHashtags.addEventListener('submit', function (evt) {
+  var hashtagsArr = getValidHashtags(inputHashtags.value.split(' '));
+  var errorCode = checkHashtag(hashtagsArr);
 
   if (errorCode) {
     inputHashtags.setCustomValidity(HASHTAG_ERRORS[errorCode]);
