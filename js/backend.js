9eb894eeb8f4fa5c61213034dbd72795695aa3e1
lyolichek <lyolya.sashevna@gmail.com>
2018-03-05 00:35:19 +0200

    Travis fixes

--- js/backend.js
+++ js/backend.js
@@ -30,30 +30,30 @@
   }
 
   function upload(data, onLoad, onError) {
-      var xhr = new XMLHttpRequest();
-      xhr.responseType = 'json';
+    var xhr = new XMLHttpRequest();
+    xhr.responseType = 'json';
 
-      xhr.addEventListener('load', function () {
-        if (xhr.status === 200) {
-          onLoad(xhr.response);
-        } else {
-          onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-        }
-      });
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
 
-      xhr.addEventListener('error', function () {
-        onError('Произошла ошибка соединения');
-      });
+    xhr.addEventListener('error', function () {
+      onError('Произошла ошибка соединения');
+    });
 
-      xhr.addEventListener('timeout', function () {
-        onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-      });
+    xhr.addEventListener('timeout', function () {
+      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
+    });
 
-      xhr.timeout = 10000;
+    xhr.timeout = 10000;
 
-      xhr.open('POST', window.utils.serverLink);
-      xhr.send(data);
-    }
+    xhr.open('POST', window.utils.serverLink);
+    xhr.send(data);
+  }
 
   window.backend = {
     load: load,
--- js/upload.js
+++ js/upload.js
@@ -91,16 +91,16 @@
     var fileName = file.name.toLowerCase();
 
     var matches = FILE_TYPES.some(function (it) {
-     return fileName.endsWith(it);
+      return fileName.endsWith(it);
     });
-    
+
     if (matches) {
       var reader = new FileReader();
-      
+
       reader.addEventListener('load', function () {
         effectImagePreview.src = reader.result;
       });
-      
+
       reader.readAsDataURL(file);
     }
     window.utils.hide(effectLevel);
